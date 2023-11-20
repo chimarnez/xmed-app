c2e6c0abe301de628506428b4c33f9af15ac3781
chimarnez <chimarnez@gmail.com>
2023-11-19 23:58:25 -0600

    Set app theme

---
+++ src/theme/index.js
@@ -0,0 +1,52 @@
+import { createTheme } from "@mui/material";
+import palette from "./palette";
+
+const theme = createTheme({
+  components: {
+    MuiListItemIcon: {
+      styleOverrides: {
+        root: {
+          color: palette.primary,
+        },
+      },
+    },
+    MuiContainer: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "transparent",
+        },
+      },
+    },
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          backgroundColor: palette.paperDarkTransparent,
+        },
+      },
+    },
+    MuiBackdrop: {
+      styleOverrides: {
+        root: {
+          backdropFilter: "blur(5px)",
+        },
+      },
+    },
+    // MuiTable: {
+    //   styleOverrides: {
+    //     root: {
+    //       webkitScro: {
+    //         width:
+    //       }
+    //     },
+    //   },
+    // },
+  },
+  palette: {
+    mode: "dark",
+    primary: {
+      main: palette.primary,
+    },
+  },
+});
+
+export default theme;
