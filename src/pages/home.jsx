015bf11d65f89149d854152fc3f8d6c8348fee29
chimarnez <chimarnez@gmail.com>
2023-11-20 13:55:51 -0600

    Remove home button

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
 import HomeNav from "../components/home-nav";
-import { Button } from "@mui/material";
 
 const HomePage = () => {
   return (
     <div>
       <HomeNav />
       <Outlet />
-      <Button>Cololololo</Button>
     </div>
   );
 };
